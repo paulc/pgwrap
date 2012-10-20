a3f79e30f1d368e0a101d6e8a6acd9346891b26e
Paul Chakravarti <paul.chakravarti@gmail.com>
2012-10-20 18:02:39 +0100

    readme

--- pgwrap/__init__.py
+++ pgwrap/__init__.py
@@ -16,12 +16,32 @@ description = """
     The 'pgwrap' module provides a simple wrapper over psycopg2 supporting a
     Python API for common sql functions.
 
-    This is not intended to 
+    This is not intended to provide an ORM-like functionality, just some basic
+    functionality to make it easier to interact with PostgreSQL from python
+    code for simple use-cases. For more complex operations direct SQL
+    access is available.
+    
+    These 'module' include:
+
+        * Simplified handling of connections/cursor
+            * Background connection pool (provided by psycopg2.pool)
+            * Cursor context handler 
+        * Python API to wrap basic SQL functionality 
+            * select,update,delete,join)
+            * These methods are implemented as extensions to the cursor
+              context handler (allowing then to be used transactionally 
+              where needed), however are also available as stand-alone 
+              methods which create an implicit cursor for simple queries
+        * Query results as dict (using psycopg2.extras.RealDictCursor)
+
+    Basic usage:
 
     >>> from pgwrap import connect,query
     >>> connect(url="postgres://localhost")
-    >>> query("select version")
-    ...
+    >>> query_one("select version()")
+    {'version': 'PostgreSQL...'}
 
+    The module wraps the excellent 'psycopg2' library and most of the 
+    functionality is provided by this behind the scenes.
 
 """
